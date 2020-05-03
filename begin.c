dbdc5fd41cc61dac7e159e6b60b5d8f5696124ce
vanshikapandey <pvanshik217@gmail.com>
2020-05-03 17:19:21 +0530

    Some more operations in C

--- begin.c
+++ begin.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c=0;
+int a,b,c=0,d=0;
 
 printf("Hello World!!!");
 printf("\n Enter two numbers:\n");
 scanf("%d %d",&a,&b);
 c=a+b;
+d=a-b;
 printf("\n Sum of the numbers is:%d",c); 
-
+printf("\n Difference of two numbers is:%d",d);
  return 0;
 }
