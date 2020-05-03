8bc62f35cd82ff8fa07f19544db35b21dcc7065a
vanshikapandey <pvanshik217@gmail.com>
2020-05-03 19:17:29 +0530

    multiplication

--- begin.c
+++ begin.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c=0,d=0,s1,s2;
+int a,b,c=0,d=0,s1,s2,p;
 
 printf("Hello World!!!");
 printf("\n Enter two numbers:\n");
 scanf("%d %d",&a,&b);
 c=a+b;
 d=a-b;
+p=a*b;
 s1=a*a;
 s2-b*b;
 printf("\n Sum of the numbers is:%d",c); 
 printf("\n Difference of two numbers is:%d",d);
 printf("\n Square of both the numbers is:%d%d",s1,s2);
+printf("\n Product of the two numbers is:%d",p);
  return 0;
 }
