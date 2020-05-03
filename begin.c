fbd03b17bbc4fc21271ac8c2bc2fbbc1af4bdb31
vanshikapandey <pvanshik217@gmail.com>
2020-05-03 19:12:09 +0530

    squaring the numbers

--- begin.c
+++ begin.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c=0,d=0;
+int a,b,c=0,d=0,s1,s2;
 
 printf("Hello World!!!");
 printf("\n Enter two numbers:\n");
 scanf("%d %d",&a,&b);
 c=a+b;
 d=a-b;
+s1=a*a;
+s2-b*b;
 printf("\n Sum of the numbers is:%d",c); 
 printf("\n Difference of two numbers is:%d",d);
+printf("\n Square of both the numbers is:%d%d",s1,s2);
  return 0;
 }
