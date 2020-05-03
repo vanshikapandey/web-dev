35433bc090e0e1d2525b4dd2609ed2a30ebf53c8
vanshikapandey <pvanshik217@gmail.com>
2020-05-03 19:24:50 +0530

    modulus

--- begin.c
+++ begin.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c=0,d=0;
+int a,b,c=0,d=0,m;
 
 printf("Hello World!!!");
 printf("\n Enter two numbers:\n");
 scanf("%d %d",&a,&b);
 c=a+b;
 d=a-b;
+m=a%b;
 printf("\n Sum of the numbers is:%d",c); 
 printf("\n Difference of two numbers is:%d",d);
+printf("\n Modulus of two numbers is:%d",m);
  return 0;
 }
