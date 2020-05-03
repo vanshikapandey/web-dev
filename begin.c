9c82e3e07f1c6a6795f6b6c433cc90e970fc3d5b
vanshikapandey <pvanshik217@gmail.com>
2020-05-03 16:52:25 +0530

    Additon of 2 numbers in C

--- begin.c
+++ begin.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
+int a,b,c=0;
+
 printf("Hello World!!!");
+printf("\n Enter two numbers:\n");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("\n Sum of the numbers is:%d",c); 
+
  return 0;
 }
